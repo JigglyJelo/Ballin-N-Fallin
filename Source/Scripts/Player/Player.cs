7cf296396d4353a74871d40f459695fe2c692de6
JigglyJello <imjigglyjello@gmail.com>
2026-08-05 16:37:11 -0500

    make controller vibration player finish stop not attempt when using mouse

--- Source/Scripts/Player/Player.cs
+++ Source/Scripts/Player/Player.cs
@@ -425,7 +425,7 @@ public partial class Player : Node2D{
 					Rb.SetDeferred("global_position",new Vector2(1000000f,Game.Random.Next(-100000,100000)));
 					Rb.SkipInterpolation();
 				}
-				if(OwnsPlayer()){
+				if(OwnsPlayer() && PlayerData.InputDevice < PlayerData.PlayerInputDevice.Mouse){
 					Input.StopJoyVibration((int)PlayerData.InputDevice);
 				}
 				Trail.ResetTrail();
