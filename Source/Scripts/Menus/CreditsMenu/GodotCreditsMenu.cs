d7c43318f65f8b8218c030537b4febcb283d2373
JigglyJello <imjigglyjello@gmail.com>
2026-08-14 16:40:58 -0500

    Improved Godot Credits Menu

--- Source/Scripts/Menus/CreditsMenu/GodotCreditsMenu.cs
+++ Source/Scripts/Menus/CreditsMenu/GodotCreditsMenu.cs
@@ -2,20 +2,19 @@ using Godot;
 using System.Text;
 
 public partial class GodotCreditsMenu : Menu{
-	[Export]
-	public float VisibleAreaHeight = 600f;
-
+	private ScrollContainer scrollContainer;
 	private Label licenseLabel;
-	private float topLimit;
+	private const float SCROLL_SPEED = 2000;
 
 	public override void _Ready(){
-		licenseLabel = GetNode<Label>("LicenseLabel");
+		scrollContainer = GetNode<ScrollContainer>("Panel/ScrollContainer");
+		licenseLabel = scrollContainer.GetNode<Label>("CreditsLabel");
+		
 		licenseLabel.Text = GetLicensesString();
-		topLimit = licenseLabel.Position.Y;
 	}
 
 	public override void _Process(double delta){
-		float bottomLimit = topLimit - Mathf.Max(0, licenseLabel.Size.Y - VisibleAreaHeight);
+		VScrollBar vScroll = scrollContainer.GetVScrollBar();
 
 		for(int i = 0; i < Game.MAX_PLAYERS; i++){
 			if(Input.IsActionJustReleased("B" + i)){
@@ -23,21 +22,14 @@ public partial class GodotCreditsMenu : Menu{
 				return;
 			}else{
 				float y = Input.GetVector("Aim Left" + i, "Aim Right" + i, "Aim Up" + i, "Aim Down" + i).Y;
-				if(y > 0.5f && licenseLabel.Position.Y > bottomLimit){
-					licenseLabel.Position -= new Vector2(0,(float)delta * 400);
+				if(y > 0.5f){
+					vScroll.Value += SCROLL_SPEED * delta;
 					return;
-				}else if(y < -0.5f && licenseLabel.Position.Y < topLimit){
-					licenseLabel.Position += new Vector2(0,(float)delta * 400);
+				}else if(y < -0.5f){
+					vScroll.Value -= SCROLL_SPEED * delta;
 					return;
 				}
 			}
-		}
-		if(Input.IsActionJustReleased("ScrollWheelUp") && licenseLabel.Position.Y < topLimit){
-			licenseLabel.Position += new Vector2(0,(float)delta * 4000);
-			return;
-		}else if(Input.IsActionJustReleased("ScrollWheelDown") && licenseLabel.Position.Y > bottomLimit){
-			licenseLabel.Position -= new Vector2(0,(float)delta * 4000);
-			return;
 		}
 	}
 
@@ -52,15 +44,15 @@ public partial class GodotCreditsMenu : Menu{
 
 	public static string GetLicensesString(){
 		StringBuilder fullText = new StringBuilder();
-		fullText.Append("Made in Godot " + Engine.GetVersionInfo()["major"] + "." + Engine.GetVersionInfo()["minor"] + "." + Engine.GetVersionInfo()["patch"]);
-		fullText.Append("========================================\n");
-		fullText.Append("          GODOT ENGINE LICENSE          \n");
-		fullText.Append("========================================\n\n");
+		fullText.Append("Made in Godot " + Engine.GetVersionInfo()["major"] + "." + Engine.GetVersionInfo()["minor"] + "." + Engine.GetVersionInfo()["patch"]+"\n");
+		fullText.Append("\n");
+		fullText.Append("GODOT ENGINE LICENSE\n");
+		fullText.Append("\n");
 		fullText.Append(Engine.GetLicenseText() + "\n\n\n");
 		
-		fullText.Append("========================================\n");
-		fullText.Append("        THIRD-PARTY LICENSES            \n");
-		fullText.Append("========================================\n\n");
+		fullText.Append("\n");
+		fullText.Append("THIRD-PARTY LICENSES\n");
+		fullText.Append("\n\n");
 		
 		Godot.Collections.Dictionary licenses = Engine.GetLicenseInfo();
 		foreach (Variant key in licenses.Keys){
@@ -68,9 +60,9 @@ public partial class GodotCreditsMenu : Menu{
 			fullText.Append((string)licenses[key] + "\n\n");
 		}
 		
-		fullText.Append("========================================\n");
-		fullText.Append("        COPYRIGHT ATTRIBUTIONS          \n");
-		fullText.Append("========================================\n\n");
+		fullText.Append("\n");
+		fullText.Append("COPYRIGHT ATTRIBUTIONS\n");
+		fullText.Append("\n\n");
 		
 		Godot.Collections.Array<Godot.Collections.Dictionary> copyrights = Engine.GetCopyrightInfo();
 		foreach(Godot.Collections.Dictionary item in copyrights){
